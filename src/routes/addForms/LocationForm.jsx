4c09fb9599813b28ac5013782842085e3a3f5cff
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-01-10 16:50:09 -0700

    Add autofocus to location

--- src/routes/addForms/LocationForm.jsx
+++ src/routes/addForms/LocationForm.jsx
@@ -14,6 +14,7 @@ return (
             <div className='border-b-2 border-emerald-700/60 flex flex-row align-center justify-start w-full'>
                 <label className='text-lg font-semibold text-gray-700'>location:</label>
                 <input
+                    autoFocus
                     required
                     type="text"
                     className="pl-3 border-none outline-none text-left text-lg"
